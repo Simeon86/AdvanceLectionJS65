e4b786813b292bedaede93622c53274ff97003a8
Simeon86 <you@example.com>
2023-04-10 11:28:27 +0300

    first commit

---
+++ Boilerplate/assets/js/app.js
@@ -0,0 +1,6 @@
+import { sumNumbers, substractNumber, divisionNumber, multiplicationNumber } from "./function.js";
+
+console.log(sumNumbers(1,2));
+console.log(substractNumber(5,3));
+console.log(divisionNumber(9,3));
+console.log(multiplicationNumber(2,4));
---
+++ Boilerplate/assets/js/function.js
@@ -0,0 +1,17 @@
+function sumNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+function substractNumber(num1, num2) {
+    return num1 - num2;
+}
+
+function divisionNumber(num1, num2) {
+    return num1 / num2;
+}
+
+function multiplicationNumber(num1, num2) {
+    return num1 * num2;
+}
+
+export {sumNumbers, substractNumber, divisionNumber, multiplicationNumber}
