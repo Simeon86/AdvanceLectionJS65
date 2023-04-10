3f5103d53da7068a4de8a6657afd46d98a7e011b
Simeon86 <you@example.com>
2023-04-10 14:48:45 +0300

    first commit

--- Boilerplate/assets/js/app.js
+++ Boilerplate/assets/js/app.js
@@ -4,3 +4,24 @@ console.log(sumNumbers(1,2));
 console.log(substractNumber(5,3));
 console.log(divisionNumber(9,3));
 console.log(multiplicationNumber(2,4));
+
+console.log('///////////////////////////////////////////////////////////////////////////////////')
+
+const modalButton = document.querySelector('#openModal');
+const modalContainer = document.querySelector('#myModal');
+
+const closeButton = modalContainer.querySelector('span.close')
+const cancelButton = modalContainer.querySelector('button.btn-cancel')
+
+modalButton.addEventListener('click', function(event) {
+    modalContainer.style.display = 'block';
+})
+
+closeButton.addEventListener('click', function(event) {
+    modalContainer.style.display = 'none';
+})
+
+cancelButton.addEventListener('click', function(event) {
+    modalContainer.style.display = 'none';
+})
+
